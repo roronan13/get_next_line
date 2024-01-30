438ab04619573c32ed45bce8708909147707e475
roronan13 <ronan.pothier@gmail.com>
2024-01-30 19:20:48 +0100

    30 jan

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,14 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 18:18:15 by rpothier          #+#    #+#             */
-/*   Updated: 2024/01/24 18:21:21 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/01/30 15:16:06 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include <unistd.h>
 
 char	*get_next_line(int fd);
 
