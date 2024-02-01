ef1e643612177ddaef4af8b6865e839ef6a35d6a
roronan13 <ronan.pothier@gmail.com>
2024-02-01 14:38:47 +0100

    1 fev

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 18:17:16 by rpothier          #+#    #+#             */
-/*   Updated: 2024/01/24 18:20:37 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/02/01 14:08:44 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 
 char	*get_next_line(int fd)
 {
-	ssize_t	read(int fd, void *buf, size_t count);
+	void	*buf;
+	size_t	count;
+	
+	read(fd, buf, count);
+
 }
