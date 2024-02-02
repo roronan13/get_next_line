3d32036fafa34f6ae117c68f4bf9a6839ca1cdee
roronan13 <ronan.pothier@gmail.com>
2024-02-02 12:45:34 +0100

    2 fev

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 18:18:15 by rpothier          #+#    #+#             */
-/*   Updated: 2024/01/30 15:16:06 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/02/02 12:44:07 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
 
 # include <unistd.h>
 
