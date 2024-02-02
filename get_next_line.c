4a9800d45f2663982cf96d0e65b7c15249e0632b
roronan13 <ronan.pothier@gmail.com>
2024-02-02 11:51:24 +0100

    2 fev

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 18:17:16 by rpothier          #+#    #+#             */
-/*   Updated: 2024/02/02 10:46:16 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/02/02 11:46:52 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 char	*get_next_line(int fd)
 {
-	void	*buf;
-	size_t	count;
-	void	*stash;
+	void		*buf;
+	size_t		count;
+	static void	*stash;
 	
+	count = 4;
 	buf = malloc(sizeof(char) * count);
 	read(fd, buf, count);
 	stash = buf;
+	
 }
