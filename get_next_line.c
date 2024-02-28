e45c9343d2cd519ade8f3278ae42f7e59e22c803
roronan13 <ronan.pothier@gmail.com>
2024-02-28 14:25:22 +0100

    28 fev

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 18:17:16 by rpothier          #+#    #+#             */
-/*   Updated: 2024/02/28 13:54:59 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/02/28 14:19:30 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ char	*fill_stash_w_read(int fd, char **stash, ssize_t nbr_read)
 		if (!buf)
 			return (free(*stash), *stash = NULL, NULL);
 		nbr_read = read(fd, buf, BUFFER_SIZE);
-		if ((!stash && nbr_read == 0) || nbr_read == -1)
+		if ((!*stash && nbr_read == 0) || nbr_read == -1)
 		{
 			if (nbr_read == -1)
 			{
